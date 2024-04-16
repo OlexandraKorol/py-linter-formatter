e9ab53bbc62b86f5ee858f586890c6824a49b48a
Olexandra Korol <korol.olexandra@gmail.com>
2024-04-16 15:31:58 +0300

    creating logic og function

--- app/main.py
+++ app/main.py
@@ -1,13 +1,43 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [{
+            "line": error["line_number"],
+            "column": error["column_number"],
+            "message": error["text"],
+            "name": error["code"],
+            "source": "flake8"
+        }
+            for error in errors],
+        "status": "passed" if not errors else "failed",
+        "path": file_path
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        {
+            "errors": [
+                {
+                    "line": error["line_number"],
+                    "column": error["column_number"],
+                    "message": error["text"],
+                    "name": error["code"],
+                    "source": "flake8"
+                }
+                for error in errors_list
+            ],
+            "status": "passed" if not errors_list else "failed",
+            "path": file_check,
+        }
+        for file_check, errors_list in linter_report.items()
+    ]
