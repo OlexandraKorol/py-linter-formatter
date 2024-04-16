8f91d3d4dc9d39d6dce320c9c72a8b06b156f7a4
Olexandra Korol <korol.olexandra@gmail.com>
2024-04-16 15:36:04 +0300

    code refactor

--- app/main.py
+++ app/main.py
@@ -10,34 +10,11 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [{
-            "line": error["line_number"],
-            "column": error["column_number"],
-            "message": error["text"],
-            "name": error["code"],
-            "source": "flake8"
-        }
-            for error in errors],
+        "errors": [format_linter_error(error) for error in errors],
         "status": "passed" if not errors else "failed",
         "path": file_path
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [
-        {
-            "errors": [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in errors_list
-            ],
-            "status": "passed" if not errors_list else "failed",
-            "path": file_check,
-        }
-        for file_check, errors_list in linter_report.items()
-    ]
+    return [format_single_linter_file(file_path, errors_list) for file_path, errors_list in linter_report.items()]
